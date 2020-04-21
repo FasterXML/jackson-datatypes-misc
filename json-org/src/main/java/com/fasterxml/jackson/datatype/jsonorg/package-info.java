c0ad0952fc943e8b70fe1baa1b81c1b94e8ecddd
Tatu Saloranta <tatu.saloranta@iki.fi>
2020-04-20 20:07:34 -0700

    Fix misleading (copy-pasted) package info

--- json-org/src/main/java/com/fasterxml/jackson/datatype/jsonorg/package-info.java
+++ json-org/src/main/java/com/fasterxml/jackson/datatype/jsonorg/package-info.java
@@ -1,10 +1,5 @@
 /**
- * Package that contains experimental implementation of
- * "Binary-Encoded JSON-Like" data format handlers (parser,
- * generator, factory produce both, supporting constants).
- *<p>
- * See <a href="http://wiki.fasterxml.com/SmileFormat">Smile format specification</a> for more details.
- * 
- * @since 1.6
+ * Package that contains support for "org.json" library
+ * datatypes for JSON content.
  */
 package com.fasterxml.jackson.datatype.jsonorg;
