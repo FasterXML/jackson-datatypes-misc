0969cbf77759db1525ef6a7b821b6dd26cffd978
Christopher Smith <chrylis@gmail.com>
2021-08-18 11:36:00 -0500

    Add datatype module for `jakarta.mail.InternetAddress`

---
+++ jakarta-mail/src/test/java/com/fasterxml/jackson/datatype/jakarta/mail/VersionTest.java
@@ -0,0 +1,34 @@
+package com.fasterxml.jackson.datatype.jakarta.mail;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+
+import org.junit.Test;
+
+import com.fasterxml.jackson.core.Version;
+import com.fasterxml.jackson.core.Versioned;
+
+public class VersionTest
+{
+    @Test
+    public void testModuleVersionAndName() throws IOException {
+        JakartaMailModule module = new JakartaMailModule();
+        assertVersion(module);
+        // just because name is assigned programmatically
+        assertEquals("jackson-datatype-jakarta-mail",
+                module.getModuleName());
+    }
+
+    /*
+    /**********************************************************
+    /* Helper methods
+    /**********************************************************
+     */
+
+    private void assertVersion(Versioned vers) {
+        final Version v = vers.version();
+        assertEquals(PackageVersion.VERSION, v);
+    }
+}
+
