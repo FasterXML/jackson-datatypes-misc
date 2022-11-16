035f5b898f349df5f824b0206ec3382c9f4e6b93
Pascal van Eck <p.vaneck@sig.eu>
2022-11-16 17:40:25 +0100

    Do not check elements of the array

--- jsr-353/src/main/java/com/fasterxml/jackson/datatype/jsr353/JsonPatchDeserializer.java
+++ jsr-353/src/main/java/com/fasterxml/jackson/datatype/jsr353/JsonPatchDeserializer.java
@@ -8,10 +8,7 @@ import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 import com.fasterxml.jackson.databind.type.LogicalType;
 
 import javax.json.Json;
-import javax.json.JsonArray;
 import javax.json.JsonPatch;
-import javax.json.JsonValue;
-import javax.json.JsonValue.ValueType;
 import java.io.IOException;
 
 public class JsonPatchDeserializer extends StdDeserializer<JsonPatch> {
@@ -31,18 +28,9 @@ public class JsonPatchDeserializer extends StdDeserializer<JsonPatch> {
     @Override
     public JsonPatch deserialize(JsonParser p, DeserializationContext ctxt) throws IOException {
         if (p.currentToken() != JsonToken.START_ARRAY) {
-            throw getException(p, p.getText());
+            throw InvalidFormatException.from(p, "JSON patch has to be an array of objects", p.getText(),
+                handledType());
         }
-        final JsonArray patch = jsonValueDeser._deserializeArray(p, ctxt);
-        for (final JsonValue element : patch.getValuesAs(JsonValue.class)) {
-            if (element.getValueType() != ValueType.OBJECT) {
-                throw getException(p, element.toString());
-            }
-        }
-        return Json.createPatch(patch);
-    }
-
-    private InvalidFormatException getException(final JsonParser p, final String value) {
-        return InvalidFormatException.from(p, "JSON patch has to be an array of objects", value, handledType());
+        return Json.createPatch(jsonValueDeser._deserializeArray(p, ctxt));
     }
 }
