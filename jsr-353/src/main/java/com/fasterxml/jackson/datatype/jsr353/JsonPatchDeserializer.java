03ecf2e407ce1601b99ac1b119ec18c20a1801a5
kostya05983 <kostya05983@mail.ru>
2023-07-12 13:55:25 +0700

    Fix class path scaning on each deserialization

--- jsr-353/src/main/java/com/fasterxml/jackson/datatype/jsr353/JsonPatchDeserializer.java
+++ jsr-353/src/main/java/com/fasterxml/jackson/datatype/jsr353/JsonPatchDeserializer.java
@@ -7,8 +7,8 @@ import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
 import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 import com.fasterxml.jackson.databind.type.LogicalType;
 
-import javax.json.Json;
 import javax.json.JsonPatch;
+import javax.json.spi.JsonProvider;
 import java.io.IOException;
 
 public class JsonPatchDeserializer extends StdDeserializer<JsonPatch> {
@@ -31,6 +31,8 @@ public class JsonPatchDeserializer extends StdDeserializer<JsonPatch> {
             throw InvalidFormatException.from(p, "JSON patch has to be an array of objects", p.getText(),
                 handledType());
         }
-        return Json.createPatch(jsonValueDeser._deserializeArray(p, ctxt));
+        return provider.createPatch(jsonValueDeser._deserializeArray(p, ctxt));
     }
+
+    private final static JsonProvider provider = JsonProvider.provider();
 }
